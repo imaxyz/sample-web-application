1da2316e9d0fbe780ff474240337ad791b16a9db
imaxyz <imaxyz@users.noreply.github.com>
2022-03-09 09:22:07 +0900

    setup.py sdistの試し実行結果

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='my-package',
+    version='0.0.1',
+    description='sample web application package description',
+    url='https://github.com/imaxyz/sample-web-application',
+    install_requires=['Django>=2.2'],
+    classifiers=[
+        'Framework :: Django',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Internet :: ',
+    ],
+)
